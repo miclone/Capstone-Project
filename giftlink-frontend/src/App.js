30e2c3a27b1817cd35ec2544182bc64651df52b3
miclone <miclone.research@gmail.com>
2024-11-16 12:02:40 +1000

    final version

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,23 +5,26 @@ import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import Profile from './components/Profile/Profile';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+
 function App() {
   const navigate = useNavigate();
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
-          <Route path="/app/profile" element={<Profile/>} />
-        </Routes>
-        </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+      </Routes>
+    </>
   );
 }
 export default App;
